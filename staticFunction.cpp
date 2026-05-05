8f1c43f01df3b0c9630d622b3fddcd1a39e7ad16
Evelofdex <evelofdex@gmail.com>
2026-05-05 18:53:33 +0700

    mengisi member class mahasiswa

--- staticFunction.cpp
+++ staticFunction.cpp
@@ -2,7 +2,18 @@
 using namespace std;
 
 class Mahasiswa{
+    private:
+    static int nim;
+    public:
+    int id;
+    string nama;
 
+    void setID();
+    void printAll();
+
+    static void setNim(int pnim){
+        nim = pnim;
+    };
 };
 
 int main(){
