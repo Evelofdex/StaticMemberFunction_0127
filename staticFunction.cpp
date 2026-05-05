28e3be575a4f84d5b84cc9c7efc327126932cade
Evelofdex <evelofdex@gmail.com>
2026-05-05 19:07:11 +0700

    mengisi kode didalam int main

--- staticFunction.cpp
+++ staticFunction.cpp
@@ -28,6 +28,22 @@ void Mahasiswa::printAll(){
 }
 
 int main(){
+    Mahasiswa mhs1("Sri Dadi");
+    Mahasiswa mhs2("Budi Jatmiko");
+
+    Mahasiswa::setNim(9);
+    Mahasiswa mhs3("Andi Janu");
+    Mahasiswa mhs4("Joko Widono");
+
+    mhs1.printAll();
+    mhs2.printAll();
+    mhs3.printAll();
+    mhs4.printAll();
+
+    cout << "Akses dari luar object = " << Mahasiswa::getNim() << endl;
+    system("pause");
+
+    return 0;
 
 }
 
