3b1bad57a87a6848180165d6720ab85ff236bb0d
Evelofdex <evelofdex@gmail.com>
2026-05-05 18:55:32 +0700

    pendefinisial prosedur2 diluar class

--- staticFunction.cpp
+++ staticFunction.cpp
@@ -16,6 +16,17 @@ class Mahasiswa{
     };
 };
 
+
+void  Mahasiswa::setID(){
+    id = ++nim;
+}
+
+void Mahasiswa::printAll(){
+    cout << "ID =   " << id << endl;
+    cout << "Nama = " << nama << endl;
+    cout << endl;
+}
+
 int main(){
 
 }
